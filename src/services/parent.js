262ab8cdf03ed3d4fc01387e35ffa9b550f0b0d9
IgorFratczak <245804@edu.p.lodz.pl>
2026-01-28 17:40:50 +0100

    fix /parent path

--- src/services/parent.js
+++ src/services/parent.js
@@ -1,142 +1,148 @@
-import {request} from "./utils/request";
+import { request } from "./utils/request";
 
 export const addChild = async (data, token) => {
-    try{
-        const formData = new FormData();
+  try {
+    const formData = new FormData();
 
-        const addChildDto = {
-            name: data.name,
-            surname: data.surname,
-            birthday: data.birthday.toISOString().split('T')[0],
-        }
+    const addChildDto = {
+      name: data.name,
+      surname: data.surname,
+      birthday: data.birthday.toISOString().split("T")[0],
+    };
 
-        formData.append('childData', new Blob([JSON.stringify(addChildDto)], {
-            type: 'application/json'
-        }));
+    formData.append(
+      "childData",
+      new Blob([JSON.stringify(addChildDto)], {
+        type: "application/json",
+      }),
+    );
 
-        formData.append('childPhoto', data.photo);
+    formData.append("childPhoto", data.photo);
 
-        return request("/add-child",{
-            method: "POST",
-            body: formData,
-            headers: {
-                "Authorization": token
-            }
-        })
-    }
-    catch(err){
-        throw err;
-    }
-}
+    return request("/parent/add-child", {
+      method: "POST",
+      body: formData,
+      headers: {
+        Authorization: token,
+      },
+    });
+  } catch (err) {
+    throw err;
+  }
+};
 
 export const addChildById = async (childId, token) => {
-    return request("/add-child-by-id",{
-        method: "POST",
-        body: JSON.stringify(childId),
-        headers: {
-            "Authorization": token
-        }
-    })
-}
+  return request("/parent/add-child-by-id", {
+    method: "POST",
+    body: JSON.stringify(childId),
+    headers: {
+      Authorization: token,
+    },
+  });
+};
 
 export const findParent = async (name, token, limit = 5) => {
-    return request(`/parent/find?name=${encodeURIComponent(name)}&limit=${limit}`, {
-        method: "GET",
-        headers: {
-            Authorization: token
-        }
-    });
+  return request(
+    `/parent/find?name=${encodeURIComponent(name)}&limit=${limit}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: token,
+      },
+    },
+  );
 };
 
 export const getAllParents = async (token) => {
-    return request("/get-all", {
-        method: "GET",
-        headers: {
-            Authorization: token
-        }
-    });
+  return request("/get-all", {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  });
 };
 
 export const changeBlockStatus = async (parentId, isLocked, token) => {
-    return request(`/${parentId}/status?isLocked=${isLocked}`, {
-        method: "PATCH",
-        headers: {
-            Authorization: token
-        }
-    });
+  return request(`/${parentId}/status?isLocked=${isLocked}`, {
+    method: "PATCH",
+    headers: {
+      Authorization: token,
+    },
+  });
 };
 
-
 export const joinClass = async (AddChildToClassDto, token) => {
-    return request("/parent/join-class",{
-        method: "POST",
-        body: JSON.stringify(AddChildToClassDto),
-        headers: {
-            "Authorization": token
-        }
-    })
-}
+  return request("/parent/join-class", {
+    method: "POST",
+    body: JSON.stringify(AddChildToClassDto),
+    headers: {
+      Authorization: token,
+    },
+  });
+};
 
 export const deleteChildFromClass = async (classId, childId, token) => {
-    const url = "/parent/delete/class/" + classId + "/child/" + childId;
+  const url = "/parent/delete/class/" + classId + "/child/" + childId;
 
-    return request(url, {
-        method: "POST",
-        headers: {
-            "Authorization": token
-        }
-    })
-}
+  return request(url, {
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+  });
+};
 
 export const changePassword = async (dto, token) => {
-    return request("/parent/change-password",{
-        method: "POST",
-        body: JSON.stringify(dto),
-        headers: {
-            "Authorization": token
-        }
-    })
-}
+  return request("/parent/change-password", {
+    method: "POST",
+    body: JSON.stringify(dto),
+    headers: {
+      Authorization: token,
+    },
+  });
+};
 
 export const editChild = async (data, token) => {
-    try{
-        const formData = new FormData();
+  try {
+    const formData = new FormData();
 
-        const addChildDto = {
-            id: data.id,
-            name: data.name,
-            surname: data.surname,
-            birthday: data.birthday.toISOString().split('T')[0],
-        }
+    const addChildDto = {
+      id: data.id,
+      name: data.name,
+      surname: data.surname,
+      birthday: data.birthday.toISOString().split("T")[0],
+    };
 
-        formData.append('editChildDto', new Blob([JSON.stringify(addChildDto)], {
-            type: 'application/json'
-        }));
+    formData.append(
+      "editChildDto",
+      new Blob([JSON.stringify(addChildDto)], {
+        type: "application/json",
+      }),
+    );
 
-        if(data.photo){
-            formData.append('childPhoto', data.photo);
-        }
-
-        return request("/parent/edit-child",{
-            method: "POST",
-            body: formData,
-            headers: {
-                "Authorization": token
-            }
-        })
+    if (data.photo) {
+      formData.append("childPhoto", data.photo);
     }
-    catch(err){
-        throw err;
-    }
-}
+
+    return request("/parent/edit-child", {
+      method: "POST",
+      body: formData,
+      headers: {
+        Authorization: token,
+      },
+    });
+  } catch (err) {
+    throw err;
+  }
+};
 
 export const deleteChild = async (childId, token) => {
-    const url = "/parent/delete-child/" + childId;
+  const url = "/parent/delete-child/" + childId;
 
-    return request(url,{
-        method: "POST",
-        headers: {
-            "Authorization": token
-        }
-    })
-}
+  return request(url, {
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+  });
+};
